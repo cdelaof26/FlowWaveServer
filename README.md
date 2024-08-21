9571febec7fac18b5baf6528c0bccd653de3e28c
cdelaof26 <cdelaof1800@alumno.ipn.mx>
2024-08-21 15:50:31 -0600

    Proyecto inicial

---
+++ README.md
@@ -0,0 +1,25 @@
+# FlowWave Server
+
+### Descripción
+Esta serie de scripts forman un servidor para la transferencia de 
+ficheros a través de WebSockets.
+
+
+### Copyright
+<pre>
+https://github.com/cdelaof26
+</pre>
+
+Por el momento, este software no contiene una licencia, por
+lo que todos los derechos están reservados - cdelaof26.
+
+
+### Dependencias
+Para el desarrollo se requiere únicamente del intérprete de Python en
+una versión superior a 3.8
+
+
+### Historial de cambios
+
+#### v0.0.1
+- Proyecto inicial
---
+++ server.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import asyncio
+import websockets
+import sys
+
+
+home = Path.home()
+
+
+def ls() -> str:
+    global home
+    data = [f'"{e.name.replace(e.suffix, "")}"' for e in home.iterdir()]
+
+    return '{"data": [' + ', '.join(data) + ']}'
+
+
+def parse_data(data: str) -> str:
+    if data == "ls":
+        return ls()
+
+    return '{"data": "Unknown command"}'
+
+
+async def echo(socket: websockets.WebSocketServerProtocol):
+    async for message in socket:
+        print(f"Received data: {message}")
+        await socket.send(parse_data(message))
+
+
+if __name__ == "__main__":
+    port = 6789
+    if len(sys.argv) == 2:
+        port = int(sys.argv[1])
+
+    start_server = websockets.serve(echo, "localhost", port)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    print("Server started at ws://localhost:6789")
+    asyncio.get_event_loop().run_forever()
